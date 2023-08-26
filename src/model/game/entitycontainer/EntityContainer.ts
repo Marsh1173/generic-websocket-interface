e6da1ffaee54b7ee69fbbb584fb91e973f2bfe61
Marsh1173 <natehroylance@gmail.com>
2023-08-26 17:00:36 -0600

    Delete entity container

--- src/model/game/entitycontainer/EntityContainer.ts
+++
@@ -1,8 +0,0 @@
-// import { Entity } from "../entitymodel/entity/Entity";
-
-// export interface IEntityContainer {
-//   remove(item: Entity): void;
-//   insert(item: Entity): void;
-//   perform_all_behaviors(elapsed_seconds: number): void;
-//   process_all_physics(elapsed_seconds: number): void;
-// }
